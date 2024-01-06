7f0d2f2c7e00680138f9bf9fdcb5dc554c72c43f
akakipertsuliani <108469353+akakipertsuliani@users.noreply.github.com>
2024-01-07 01:24:35 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     window.addEventListener("scroll", () => {
-        document.getElementById("cutter1").style.top = `${window.scrollY}px`;
-        document.getElementById("cutter2").style.bottom = `${window.scrollY}px`;
+        document.getElementById("cutter1").style.bottom = `${window.scrollY}px`;
+        document.getElementById("cutter2").style.top = `${window.scrollY}px`;
+        document.getElementById("cutter3").style.bottom = `${window.scrollY}px`;
+
+        document.getElementById("cutter-2-1").style.top = `${window.innerHeight - window.scrollY}px`;
+        document.getElementById("cutter-2-2").style.bottom = `${window.innerHeight - window.scrollY}px`;
+        document.getElementById("cutter-2-3").style.top = `${window.innerHeight - window.scrollY}px`;
     });
 });

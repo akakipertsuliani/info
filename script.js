1998b70e3fde8afa2d16fd6d8978f9500513a53a
akakipertsuliani <108469353+akakipertsuliani@users.noreply.github.com>
2024-01-08 21:12:30 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
     window.addEventListener("scroll", () => {
-        document.getElementById("cutter1").style.bottom = `${window.scrollY}px`;
-        document.getElementById("cutter2").style.top = `${window.scrollY}px`;
-        document.getElementById("cutter3").style.bottom = `${window.scrollY}px`;
+        let scrollY = window.scrollY;
 
-        document.getElementById("cutter-2-1").style.top = `${window.innerHeight - window.scrollY}px`;
-        document.getElementById("cutter-2-2").style.bottom = `${window.innerHeight - window.scrollY}px`;
-        document.getElementById("cutter-2-3").style.top = `${window.innerHeight - window.scrollY}px`;
+        scrollY > window.innerHeight ? scrollY = window.innerHeight : 0;
+
+        document.getElementById("cutter1").style.bottom = `${scrollY}px`;
+        document.getElementById("cutter2").style.top = `${scrollY}px`;
+        document.getElementById("cutter3").style.bottom = `${scrollY}px`;
+
+        document.getElementById("cutter-2-1").style.top = `${window.innerHeight - scrollY}px`;
+        document.getElementById("cutter-2-2").style.bottom = `${window.innerHeight - scrollY}px`;
+        document.getElementById("cutter-2-3").style.top = `${window.innerHeight - scrollY}px`;
+
     });
 });
+
+
+const races = document.querySelector(".wrapper");
+console.log(races.offsetWidth)
+
+function getScrollAmount() {
+    let racesWidth = races.scrollWidth;
+    return -(racesWidth - window.innerWidth);
+}
+
+const tween = gsap.to(races, {
+    x: getScrollAmount,
+    duration: 3,
+    ease: "none",
+});
+
+
+ScrollTrigger.create({
+    trigger: ".zewrapper",
+    start: "top top",
+    end: () => `+=${getScrollAmount() * -1}`,
+    pin: true,
+    animation: tween,
+    scrub: 1,
+    invalidateOnRefresh: true,
+})
+
